d84c4082727e65c4e210e6b6c97f388e45c3f030
sirikoduru24 <81694764+sirikoduru24@users.noreply.github.com>
2021-05-25 20:51:30 -0700

    Changes made to Maps

--- usajobs/src/API/mapData.js
+++ usajobs/src/API/mapData.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-class mapData {
-  baseUrl = "https://code.highcharts.com/mapdata/";
-
-  getWorld = async () => {
-    return await axios.get(this.baseUrl + "countries/us/us-all.geo.json");
-  };
+function  mapData() {
+    let baseUrl = "https://code.highcharts.com/mapdata/";
+    return axios.get(baseUrl + "countries/us/us-all.geo.json");
 }
 
 export default mapData;
--- usajobs/src/Components/Map.js
+++ usajobs/src/Components/Map.js
@@ -1,163 +1,137 @@
 import React from "react";
-import mapData from "../API/mapData";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import jobsData from "../API/jobsData";
 
 require("highcharts/modules/map")(Highcharts);
 
-class MyMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mapData: null
-    };
-    // init to get the map data from api
-    this.mapData = new mapData();
-    //console.log(this.mapData);
-    this.jobsData = jobsData();
-    
-    console.log(this.jobsData);
-
-    this.jobsData.then(response => {
-        console.log(response);
-    } )
-    /*var positions = this.jobsData.then(response => { 
-      return response
-     })
-    console.log("Position array: ",positions)*/
-    //console.log(this.jobs)
-    // preparing the config of map with empty data
-    this.options = {
-      title: {
-        text: "",
-        style: {
-          color: "#fff"
-        }
-      },
-      chart: {
-        backgroundColor: "transparent",
-        type: "map",
-        map: null
-      },
-      mapNavigation: {
-        enabled: true,
-        enableButtons: false
-      },
-      credits: {
-        enabled: false
-      },
-      colorAxis: {
-        dataClasses: [
-          {
-            from: 1,
-            color: "#C40401",
-            name: "widget name one"
-          },
-          {
-            from: 2,
-            color: "#0200D0",
-            name: "widget name two"
+function Maps(props) {
+  const options = {
+    title: {
+      text: "JOBS DASHBOARD",
+      style: {
+        color: "#fff"
+      }
+    },
+    chart: {
+      backgroundColor: "transparent",
+      type: "map",
+      map: null
+    },
+    mapNavigation: {
+      enabled: true,
+      enableButtons: false
+    },
+    credits: {
+      enabled: false
+    },
+    colorAxis: {
+      minimum: 0
+    },
+    tooltip: {
+      pointFormatter: function () {
+        return '<div><div>'+this.name+'</div><div>'+this.value+'</div></div>'
+      }
+    },
+    legend: {
+      align: "bottom",
+      horizontalAlign: "left",
+      x: 50,
+      y: 70,
+      floating: true,
+      layout: "horizontal",
+      valueDecimals: 0,
+      backgroundColor:
+        // theme
+        (Highcharts.defaultOptions &&
+          Highcharts.defaultOptions.legend &&
+          Highcharts.defaultOptions.legend.backgroundColor) ||
+        "rgba(255, 255, 255, 0.85)"
+    },
+    series: [
+      {
+        name: "Available Jobs",
+        dataLabels: {
+          enabled: true,
+          color: "#FFFFFF",
+          format: "{point.postal-code}",
+          style: {
+            textTransform: "uppercase"
           }
-        ]
-      },
-      tooltip: {
-        pointFormatter: function () {
-          return this.name;
-        }
-      },
-      legend: {
-        align: "right",
-        verticalAlign: "top",
-        x: -100,
-        y: 70,
-        floating: true,
-        layout: "vertical",
-        valueDecimals: 0,
-        backgroundColor:
-          // theme
-          (Highcharts.defaultOptions &&
-            Highcharts.defaultOptions.legend &&
-            Highcharts.defaultOptions.legend.backgroundColor) ||
-          "rgba(255, 255, 255, 0.85)"
-      },
-      series: [
-        {
-          name: "Available Jobs",
-          dataLabels: {
-            enabled: true,
-            color: "#FFFFFF",
-            format: "{point.postal-code}",
-            style: {
-              textTransform: "uppercase"
-            }
-          },
-          tooltip: {
-            ySuffix: " %"
-          },
-          cursor: "pointer",
-          joinBy: "postal-code",
-          data: [],
-          point: {
-            events: {
-              click: function (r) {
-                console.log("click - to open popup as 2nd step");
-                console.log(r);
-              }
+        },
+        tooltip: {
+          ySuffix: " %"
+        },
+        cursor: "pointer",
+        joinBy: "postal-code",
+        data: [],
+        point: {
+          events: {
+            click: function (r) {
+              console.log("click - to open popup as 2nd step");
+              console.log(r,this.code);
             }
           }
         }
-      ]
-    };
-
-    // get the world map data
-    this.mapData.getWorld().then((r) => {
-      this.setState({ mapData: r.data }, () => {
-        this.options.series[0].data = []; //make sure data is empty before  fill
-        this.options["chart"]["map"] = this.state.mapData; // set the map data of the graph (using the world graph)
-
-        // filling up some dummy data with values 1 and 2
-        // instead of using the google sheet
-        for (let i in this.state.mapData["features"]) {
-          let mapInfo = this.state.mapData["features"][i];
-          if (mapInfo["id"]) {
-            var postalCode = mapInfo.properties["postal-code"];
-
-            var name = mapInfo["properties"]["name"];
-            var value = (i % 2) + 1;
-            var type = value === 1 ? "widget name one" : "widget name two";
-            var row = i;
-            //console.log(name);
-            this.options.series[0].data.push({
-              value: value,
-              name: name,
-              "postal-code": postalCode,
-              row: row,
-              type: type
-            });
-          }
-        }
-        // updating the map options
-        this.setState({ mapOptions: this.options });
-      });
-    });
+      }
+    ]
+  };
+  const renderMapsData = () => {
+    if(props.mapdata && props.jobdata) {
+      console.log(props.jobdata)
+      options.series[0].data = [];
+      options["chart"]["map"] = props.mapdata.data;
+      let x = props.jobdata
+          let dataDict = []
+          x.forEach(element => {
+            let k = {}
+            k['countryCode'] = element.locations
+              dataDict.push(k)
+          });
+          console.log('Data :',dataDict)
+          const items = dataDict.reduce((total, item) => {
+            total.hasOwnProperty(item.countryCode)
+              ? total[item.countryCode]++
+              : (total[item.countryCode] = 1);
+        
+            return total;
+          }, {});
+          console.log('Items:',items)
+          for (let i in props.mapdata.data["features"]) {
+            let mapInfo = props.mapdata.data["features"][i];
+            if (mapInfo["id"]) {
+              var postalCode = mapInfo.properties["postal-code"];
+              var name = mapInfo["properties"]["name"];
+              console.log('Name',name)
+              var code = mapInfo["properties"]["hc-key"]
+              var value = items[name];
+              var row = i;
+              options.series[0].data.push({
+                value: value,
+                name: name,
+                "postal-code": postalCode,
+                row: row,
+                code: code
+              });
+            }
+          }   
+        return(<div>
+          {options ? (
+            <HighchartsReact
+              highcharts={Highcharts}
+              constructorType={"mapChart"}
+              options={options}
+            />
+          ) : (
+            ""
+          )}
+        </div>) 
+    }
   }
-
-  render() {
-    return (
-      <div>
-        {this.state.mapOptions ? (
-          <HighchartsReact
-            highcharts={Highcharts}
-            constructorType={"mapChart"}
-            options={this.state.mapOptions}
-          />
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
+return (
+  <div>
+    {renderMapsData()}
+  </div>
+)
 }
 
-export default MyMap;
+export default Maps
