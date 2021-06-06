9b636b55675cbc7048f22352c4f970b4583fbf55
ruchakulkarni1 <rucha2@pdx.edu>
2021-06-05 19:43:42 -0700

    Updates

--- usajobs/src/API/mapData.js
+++
@@ -1,9 +0,0 @@
-/* This function takes the data from highcharts maps API using axios and then returns it.*/
-import axios from "axios";
-
-function  mapData(extension) {
-    let baseUrl = "https://code.highcharts.com/mapdata/";
-    return axios.get(baseUrl + extension);
-}
-
-export default mapData;
