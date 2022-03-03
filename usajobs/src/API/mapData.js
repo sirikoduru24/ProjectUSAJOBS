6ea1d57889d84c1f3d8e8233dd55d9cce38bc9ac
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-03 14:30:56 -0800

    Modified comment, variable names, error handling in API/mapData.js

--- usajobs/src/API/mapData.js
+++ usajobs/src/API/mapData.js
@@ -1,9 +1,21 @@
 /* This function takes the data from highcharts maps API using axios and then returns it.*/
+/**
+ * mapData() function fetches data from highcharts.
+ * This data is being used to display USA maps on dashboard.
+ * @param extension
+ * @returns chart
+ */
+
 import axios from "axios";
 
 function  mapData(extension) {
     let baseUrl = "https://code.highcharts.com/mapdata/";
-    return axios.get(baseUrl + extension);
+    try {
+        let chart = axios.get(baseUrl + extension);
+        return chart;
+    } catch {
+        console.error("Error fetching chart data")
+    }
 }
 
 export default mapData;
