bf45a25a7baafa1847d95b5549df5eabb02d66cc
sirikoduru24 <siri4@pdx.edu>
2022-03-10 15:33:46 -0800

    changes in app.js

--- usajobs/src/App.js
+++ usajobs/src/App.js
@@ -44,36 +44,33 @@ function App() {
 
 
   useEffect( () => {
-    /* This function is to get the job data from jobsData.js as an array*/
     const getJobsData = async () => {
-      const data1 = await jobsData('Page=1&ResultsPerPage=1000')
-      const data2 = await jobsData('Page=2&ResultsPerPage=1000')
-      const data3 = await jobsData('Page=3&ResultsPerPage=1000')
-      let res = data1.concat(data2,data3)
-      setJobsData(res)
+      const dataFromPage1 = await jobsData('Page=1&ResultsPerPage=1000')
+      const dataFromPage2 = await jobsData('Page=2&ResultsPerPage=1000')
+      const dataFromPage3 = await jobsData('Page=3&ResultsPerPage=1000')
+      const result = dataFromPage1.concat(dataFromPage2,dataFromPage3)
+      setJobsData(result)
     }
     getJobsData()
   },[])
 
   useEffect( () => {
-    /*This function is to get remunerations from all the jobs based on states. 
-    This function returns a dictionary of state name, minimum remuneration and maximum remuneration.*/
     const getRemunerations = async () => {
-      let jobsCountArray = []
+      let statesRemunerationData = []
       if (jobData){
       jobData.forEach(element => {
-        let dict = {}
+        let remunerationDict = {}
         
         dict['countryCode'] = element.locations
-        let maxRem = parseFloat(element.remuneration.MaximumRange)
-        let minRem = parseFloat(element.remuneration.MinimumRange)
+        let maxRemuneration = parseFloat(element.remuneration.MaximumRange)
+        let minRemuneration = parseFloat(element.remuneration.MinimumRange)
         if(element.remuneration.RateIntervalCode === "Per Hour") {
-            minRem = minRem*8*30*12
-            maxRem = maxRem*8*30*12
+          minRemuneration = minRem*8*30*12
+          maxRemuneration = maxRem*8*30*12
         }
-        dict['minRem'] = Math.floor(minRem)
-        dict['maxRem'] = Math.floor(maxRem)
-        jobsCountArray.push(dict)
+        remunerationDict['minRemuneration'] = Math.floor(minRemuneration)
+        remunerationDict['maxRemuneration'] = Math.floor(maxRemuneration)
+        statesRemunerationData.push(remunerationDict)
       });
 
     const groupBy = (array, key) => {
@@ -85,26 +82,26 @@ function App() {
     }, {});
   };
   
-  const groupByCity = groupBy(jobsCountArray, 'countryCode');
-  let finalArr = []
+  const groupByCity = groupBy(statesRemunerationData, 'countryCode');
+  let finalRemunerationArray = []
   for(const [key,value] of Object.entries(groupByCity)) {
     let min = 1000000, max = 0
     value.forEach(elem => {
-        if(min > elem.minRem) {
-            min = elem.minRem
+        if(min > elem.minRemuneration) {
+            min = elem.minRemuneration
         }
-        if(max < elem.maxRem) {
-            max = elem.maxRem
+        if(max < elem.maxRemuneration) {
+            max = elem.maxRemuneration
         }
     });
-    let dict = {}
-    dict['state'] = key
-    dict['minRem'] = min
-    dict['maxRem'] = max
-    finalArr.push(dict)
+    let remunerationsDict = {}
+    remunerationsDict['state'] = key
+    remunerationsDict['minRemuneration'] = min
+    remunerationsDict['minRemuneration'] = max
+    finalRemunerationArray.push(remunerationsDict)
 
       }
-      setRemunerationData(finalArr)
+      setRemunerationData(finalRemunerationArray)
     }}
     getRemunerations()
 
@@ -126,85 +123,72 @@ function App() {
           }, {});
         };
         
-        let result = []
-        let fieldfilter = []
-          let i=0
-          let dict = {}
-          
-          let count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0, count9 = 0, count10 = 0
-          jobData.forEach(element => {
-              if(element.positionTitle.includes("Military")) {
-                count1 = count1 + 1  
-                
-              }
-              if(element.positionTitle.includes("Medicine") || element.positionTitle.includes("Nurse")) {
-                count2 = count2 + 1
-              }
-              if(element.positionTitle.includes("Accounting") || element.positionTitle.includes("Finance") || element.positionTitle.includes("Financial")) {
-                count3 = count3 + 1
-              }
-              if(element.positionTitle.includes("IT") || element.positionTitle.includes("Engineer") || element.positionTitle.includes("Engineering") ) {
-                count4 = count4 + 1
-              }
-              if(element.positionTitle.includes("Cook") || element.positionTitle.includes("Food")) {
-                count5 = count5 + 1
-              }
-              if(element.positionTitle.includes("Attorney") || element.positionTitle.includes("Law")) {
-                count6 = count6 + 1
-              }
-              if(element.positionTitle.includes("Teach")) {
-                count7 = count7 + 1  
-              }
-              if(element.positionTitle.includes("Social Worker") || element.positionTitle.includes("Counselling")) {
-                count8 = count8 + 1  
-              }
-              
-          });
-         
-          dict['Field1'] = count1
-          dict['Field2'] = count2
-          dict['Field3'] = count3
-          dict['Field4'] = count4
-          dict['Field5'] = count5
-          dict['Field6'] = count6
-          dict['Field7'] = count7
-          dict['Field8'] = count8
-          if(count1 > 0 )
-          {
-            dict['FieldName1'] = "Military Services"
-          }
-          if(count2 > 0 )
-          {
-            dict['FieldName2'] = "Medical Services"
-          }
-          if(count3 > 0 )
-          {
-            dict['FieldName3'] = "Finance & Accounting"
-          }
-          if(count4 > 0 )
-          {
-            dict['FieldName4'] = "Engineering"
-          }
-          if(count5 > 0 )
-          {
-            dict['FieldName5'] = "Food Industry"
-          }
-          if(count6 > 0 )
-          {
-            dict['FieldName6'] = "Law & Justice"
-          }
-          if(count7 > 0 )
-          {
-            dict['FieldName7'] = "Teaching"
-          }
-          if(count8 > 0 )
-          {
-            dict['FieldName8'] = "Social Work"
-          }
-          
-          result.push(dict)
+      let result = []
+      let dict = {}
+      let count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0
+      
+      jobData.forEach(element => {
+        if(element.positionTitle.includes("Military")) {
+          count1 = count1 + 1  
+        }
+        if(element.positionTitle.includes("Medicine") || element.positionTitle.includes("Nurse")) {
+          count2 = count2 + 1
+        }
+        if(element.positionTitle.includes("Accounting") || element.positionTitle.includes("Finance") || element.positionTitle.includes("Financial")) {
+          count3 = count3 + 1
+        }
+        if(element.positionTitle.includes("IT") || element.positionTitle.includes("Engineer") || element.positionTitle.includes("Engineering") ) {
+          count4 = count4 + 1
+        }
+        if(element.positionTitle.includes("Cook") || element.positionTitle.includes("Food")) {
+          count5 = count5 + 1
+        }
+        if(element.positionTitle.includes("Attorney") || element.positionTitle.includes("Law")) {
+          count6 = count6 + 1
+        }
+        if(element.positionTitle.includes("Teach")) {
+          count7 = count7 + 1  
+        }
+        if(element.positionTitle.includes("Social Worker") || element.positionTitle.includes("Counselling")) {
+          count8 = count8 + 1  
+        }
+      });
         
-        setFieldData1(result)
+      dict['Field1'] = count1
+      dict['Field2'] = count2
+      dict['Field3'] = count3
+      dict['Field4'] = count4
+      dict['Field5'] = count5
+      dict['Field6'] = count6
+      dict['Field7'] = count7
+      dict['Field8'] = count8
+      if(count1 > 0 ) {
+        dict['FieldName1'] = "Military Services"
+      }
+      if(count2 > 0 ) {
+        dict['FieldName2'] = "Medical Services"
+      }
+      if(count3 > 0 ) {
+        dict['FieldName3'] = "Finance & Accounting"
+      }
+      if(count4 > 0 ) {
+        dict['FieldName4'] = "Engineering"
+      }
+      if(count5 > 0 ) {
+        dict['FieldName5'] = "Food Industry"
+      }
+      if(count6 > 0 ) {
+        dict['FieldName6'] = "Law & Justice"
+      }
+      if(count7 > 0 ) {
+        dict['FieldName7'] = "Teaching"
+      }
+      if(count8 > 0 ) {
+        dict['FieldName8'] = "Social Work"
+      }
+      
+      result.push(dict)
+      setFieldData1(result)
       }
     }
     getFieldData()
